6a6caf5ee1e151e7ee6df57bed06c4fd4b3b8298
JacekStrzelecki <jstrzelecki88@gmail.com>
2024-07-12 15:19:35 +0200

    Updating setup.py an requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python-e2e-localstack-tests',
+    name='jstrzelecki_app_tests',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
